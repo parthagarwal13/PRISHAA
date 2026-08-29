913653c3ade6ecc22182bc44deaec29ac14b0d37
Parth Agarwal <parthagarwal1303@gmail.com>
2026-08-30 03:37:38 +0530

    Restore stable PRISHAA version

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
-import app from "./backend/server.js";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+// Import your existing backend routes/app
+const { default: backendApp } = await import("./backend/server.js");
+
+// Mount existing application
+app.use(backendApp);
 
 export default app;
